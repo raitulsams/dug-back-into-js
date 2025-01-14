17f6decdb6280d066f72feb97f15cab428d4366c
raitulsams <raitulsams202@gmail.com>
2025-01-14 04:33:53 +0100

    square numbers

---
+++ squareNumber.js
@@ -0,0 +1,12 @@
+function sqr() {
+    for (let i = 1; i <= 100; i++) {
+        if (i !== 1 && Math.sqrt(i) % 1 === 0) { // Check if the number is a perfect square
+            console.log("First square number encountered:", i);
+            break;
+        }
+    }
+
+
+
+}
+sqr();
