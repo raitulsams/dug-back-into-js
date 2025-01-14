5470251b70b10db4d21aa69e0c250c1920edefce
raitulsams <raitulsams202@gmail.com>
2025-01-14 03:59:06 +0100

    while loop looping

---
+++ 1century.js
@@ -0,0 +1,6 @@
+let x = 1;
+for (let i = 1; i <= 200; i++) {
+    if (i === 100) {
+        break;
+    }
+}
---
+++ 2sumupto100.js
@@ -0,0 +1,10 @@
+let sum = 1
+let i = 0;
+while (true) {
+    sum = sum + i;
+    i++;
+    if (sum >= 100) {
+        break;
+    }
+    console.log(sum);
+}
