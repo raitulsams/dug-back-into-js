61649c42380da423ada025d8c1c469df97869841
raitulsams <raitulsams202@gmail.com>
2025-01-17 07:06:55 +0100

    sort() comparing function

---
+++ arrString.js
@@ -0,0 +1,5 @@
+let str = "Hello World!";
+let arr = str.split("");  // Convert string to array of characters
+
+console.log(arr)
+console.log(arr.join("")); 
---
+++ sorting.js
@@ -0,0 +1,7 @@
+let thisArr = [12, 3, 4, 1, 2]
+let newArr = thisArr.sort(function (a, b) {
+    // console.log(a, b)
+    return a - b
+
+});
+console.log(newArr)
